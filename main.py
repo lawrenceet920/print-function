ed87e72947887ffc259760993d131bc0204dc2ad
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-09 13:55:41 -0400

    did practice projects

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello world!')
+# Ethan Lawrence
+# 9 Sept 2024
+# Printing in python
+
+    # Exercise 2 - 3 (Personal Message)
+
+fullName = 'Ethan Lawrence'
+print(f'Hello {fullName} would you like to learn python today \n')
+
+    # Exercise 2 - 5 (Famous Quote)
+
+print('Albert Einstien once said "A person who nevermade a mistake has never tried anything new."\n')
+
+    # Exercise 2 - 6 (Famous Quote 2)
+
+famedPerson = 'Albert Einstien'
+message = 'once said "A person who nevermade a mistake has never tried anything new."'
+
+print(famedPerson + ' ' + message)
---
+++ prep.py
@@ -0,0 +1,8 @@
+print('Hello world!')
+
+greeting1 = 'Good morning'
+greeting2 = 'good afternoon'
+
+print(greeting1)
+print('\n\n')
+print(greeting2)
